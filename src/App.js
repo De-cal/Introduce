4d7bb8044d96645cb97927384d0b1e9f0d9597fe
Decal <vumublne@naver.com>
2025-02-25 17:24:12 +0900

    modify style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { MyName } from "./components/MyName"
 import { MyMbti } from "./components/MyMbti"
 import { MyIntro } from "./components/MyIntro"
 import { LikeImg } from "./components/LikeImg"
+import './App.css'
 
 function App() {
   return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+    <div className="container">
       <MyName name="이태빈" />
       <MyMbti mbti="INTJ" />
       <MyIntro intro="풀스택 6기 스프린트 수강생입니다."/>
--- src/components/LikeImg.js
+++ src/components/LikeImg.js
@@ -1,6 +1,6 @@
 export const LikeImg = ({ img }) => {
   return (
-    <div>
+    <div className="img">
       <span>좋아하는 사진 : </span>
       <img src={img} alt="코드잇 이미지" width="100px" />
     </div>
