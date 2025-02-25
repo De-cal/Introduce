2f0695dd310a90f3f89157df7e05f24e28cebdc1
Decal <vumublne@naver.com>
2025-02-25 17:38:05 +0900

    modify background-color

--- src/components/LikeImg.js
+++ src/components/LikeImg.js
@@ -1,7 +1,7 @@
 export const LikeImg = ({ img }) => {
   return (
-    <div className="img">
-      <span>좋아하는 사진 : </span>
+    <div>
+      <span>사진 : </span>
       <img src={img} alt="코드잇 이미지" width="100px" />
     </div>
   )
